e3af9fef6cd3dbb4de0d549745c4d9c6e3935644
Dola Barai <dola43dc@gmail.com>
2023-08-20 23:30:25 +0600

    link added

--- src/Pages/Form.jsx
+++ src/Pages/Form.jsx
@@ -7,7 +7,7 @@ const Form = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
     const onSubmit = user => {
         console.log(user);
-        fetch('http://localhost:5000/users', {
+        fetch('https://formtastic-server.vercel.app/users', {
             method: 'POST',
             headers: {
                 'content-type': 'application/json'
